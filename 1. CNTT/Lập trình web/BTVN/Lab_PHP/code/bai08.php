12c3322c6663c1ef77671f4d4e178017682e1a46
Harry1720 <22h1120002@ut.edu.vn>
2025-07-28 23:15:41 +0700

    update WEB+SQL

--- 1. CNTT/Lập trình web/BTVN/Lab_PHP/code/bai08.php
+++
@@ -1,9 +0,0 @@
-<?php
-$count = 0;
-for($i=2839; $i<=7827;$i++){
-    if($i%13 == 0){
-        $count++;
-    }
-};
-echo "Số từ 2839 đến 7827 có $count số chia hết cho 13.";
-?>
--- 1. CNTT/Lập trình web/BTVN/js-elearning/Bai_2/checksearch.js
+++
@@ -1,8 +0,0 @@
-function checksearch() {
-    if (document.formtim.tukhoa.value == "Tìm kiếm") {
-        alert("Bạn chưa nhập từ khóa tìm kiếm...");
-        document.formtim.tukhoa.focus();
-        return false; //Chặn form không cho submit
-    }
-    return true; //Cho submit form khi đã kiểm tra xong
-}
